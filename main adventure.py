ab545cf7b5870749c41c6afdbca44a3dcede9be3
Joshua Nelson <jyn514@users.noreply.github.com>
2017-01-03 05:55:41 -0500

    added delete function, formatting
    added line breaks for long text
    deleted now irrelevant comment on line 1
    added delete function for lists
    more synonyms in action list
    fixed light function (previously, reverted flashlight to boolean)

--- main adventure.py
+++ main adventure.py
@@ -1,7 +1,3 @@
-#redoing my text adventure because the other one is all over the place
- 
-#redoing my text adventure because the other one is all over the place
- 
 from time import sleep
 
 class room:
@@ -21,7 +17,8 @@ class item:
     def __init__(self, name):
         self.name=name
         self.desc = ""
-        
+
+     
 def look(lit):
     if lit == 0:
         print(location.desc_dark)
@@ -32,7 +29,7 @@ def light(lit):
     if lit == 0:
         print('You turn on your flashlight.')
         print(location.desc_light)
-        return 1       
+        return 1     
     elif lit == 1:
         print("You turn your flashlight off. You can't see for a moment as your eyes adjust.")
         sleep(3)
@@ -41,31 +38,42 @@ def light(lit):
     else:
         print('flashlight error')
 
+      
 room1=room('entrance', 0, 0)
-room1.desc_dark = 'You find yourself in a dimly lit room. You can make out a closed door, where light is creeping through.'
+room1.desc_dark = ('You find yourself in a dimly lit room. You can make out a closed door, where light is ' +
+                       'creeping through.')
 room1.desc_light = 'You are in a wooden shack.'
 
 corridor1=room('tunnel', 0, 1)
-corridor1.desc_dark = 'You see brighter light - it seems to be shining through cracks in the walls. You hear birds chirping.'
-corridor1.desc_light = 'You are in a wooden corridor made with crudely cut planks. There is a dirt floor, and you hear birds chirping.'
+corridor1.desc_dark = ('You see brighter light - it seems to be shining through cracks in the walls. ' +
+                           'You hear birds chirping.')
+corridor1.desc_light = ('You are in a wooden corridor made with crudely cut planks. There is a dirt ' +
+                            'floor, and you hear birds chirping.')
 
 room2=room('in development', 0, 2)
 room2.desc_dark = "In development."
 room2.desc_light = "In development."
 
 flashlight = item("flashlight")
-flashlight.desc = "It's a flashlight. It's bright yellow color and the light is a little dim, but it's enough to make out your surroundings."
+flashlight.desc = ("It's a flashlight. It's bright yellow color and the light is a little dim, but it's enough" +
+                       " to make out your surroundings.")
 flashlight.state = 0
 
 location = room1
 door1=0
 #Boolean, door starts closed
 inventory=[flashlight]
-actions=['location', 'look', 'inventory', 'light', 'light flashlight', 'leave', 'exit', 'forward', 'flashlight', 'Flashlight', "inspect flashlight"]
+
+actions=['location', 'look', 'inventory', 'light', 'light flashlight', 'leave', 'exit', 'forward', 'flashlight',
+         'Flashlight', "inspect flashlight", "enter"]
+def delete(list, x):
+    if x in list:
+        x=[]
+#needs to be dictionary, not list. will fix when i have time to figure it out
+
 
 
 print(room1.desc_dark)
-
 while True:
     actions.append('open door')
     actions.append('open')
@@ -73,7 +81,7 @@ while True:
         command = input ('>>')
         if (command =='open door') or (command == 'open'):
             door1=1
-            print('You open the door to the shack. Light dimly shines from the corridor ahead')
+            print('You open the door to the shack. Light dimly shines from the corridor ahead.')
         elif command == 'location':
             print(location.name)    
         elif command == 'look':
@@ -81,9 +89,10 @@ while True:
         elif command =='inventory':
             for x in inventory:
                 print(x.name)
-        elif (command == 'light') or (command=='light flashlight'):
-            flashlight = light(flashlight.state) 
-        for x in ['leave', 'exit', 'forward',]:
+        for x in ['light', 'light flashlight', "turn on", "turn on light"]:
+            if command == x:
+                flashlight.state = light(flashlight.state) 
+        for x in ['leave', 'exit', 'forward', 'enter']:
             if command==x:
                 if door1==0:
                     print('You hit your head on the door! Ouch.')
@@ -101,12 +110,11 @@ while True:
         for x in inventory:
             if (command == x.name) or (command == "inspect " + x.name):
                 print(x.desc)
-#want this to work for both upper and lower case, not sure how
         if command not in actions:
             print("Sorry, I don't recognize that word.")
         
-    actions.delete('open door')
-    actions.delete('open')
+    delete(actions, 'open door')
+    delete(actions, 'open')
     while location == corridor1:
         command = input ('>>')
     print('Out of content. I guess that counts as beating the game?')
